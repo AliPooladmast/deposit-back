bf57c79625e75fef13e00bb2a0f55560e1dea5e7
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2023-03-13 14:09:35 +0330

    add error middleware handler

--- startup/routes.js
+++ startup/routes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const authRoute = require("../routes/auth");
 const userRoute = require("../routes/user");
 const productRoute = require("../routes/product");
+const error = require("../middleware/error");
 
 module.exports = function (app) {
   app.use(express.json());
   app.use("/api/auth", authRoute);
   app.use("/api/users", userRoute);
   app.use("/api/products", productRoute);
+  app.use(error);
 };
